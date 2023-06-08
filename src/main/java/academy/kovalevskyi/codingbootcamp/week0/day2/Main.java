7b7d41e4ae3fb5c78e4d6077237d405678a2c6dd
Your Name <you@example.com>
2023-06-08 19:30:52 +0300

    fff

--- src/main/java/academy/kovalevskyi/codingbootcamp/week0/day2/Main.java
+++ src/main/java/academy/kovalevskyi/codingbootcamp/week0/day2/Main.java
@@ -2,6 +2,6 @@ package academy.kovalevskyi.codingbootcamp.week0.day2;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    System.out.println("Hello World555");
   }
 }
